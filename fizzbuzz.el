2bb6d489e0011078401e48dd4e0d803bbeda1d1e
Bart Lantz <bartlantz@gmail.com>
2010-11-22 10:39:42 -0700

    fizzbuzz.el update

--- fizzbuzz.el
+++ fizzbuzz.el
@@ -1,42 +1,16 @@
-;;(setq num 1)
-;;(if (and (< (mod num 3) 1) (< (mod num 5) 1)) (princ "fizzbuzz"))
-;;(1+ num)
-;;(princ num)
+;; Fizzbuzz written in Emacs Lisp
+;;
+;;
+;;
 
 (defun fizzbuzz (num)
-  (princ num)
-  (if (< num 100)
-      (cond 
-       ((and (< (mod num 3) 1) (< (mod num 5) 1)) (princ "fizzbuzz"))
-       ((< (mod num 5) 1) (princ "fizz"))
-       ((< (mod num 3) 1) (princ "buzz"))
-       ((princ num)))
-      (setq num (+ num 1))
-      (fizzbuzz num))
-  (princ '("nothin doin" num)))
-(fizzbuzz 11)
-
-
-
-(defun fizzbuzz (num)
-  (cond 
-   ((and (< (mod num 3) 1) (< (mod num 5) 1)) (princ "fizzbuzz"))
-   ((< (mod num 5) 1) (princ "fizz"))
-   ((< (mod num 3) 1) (princ "buzz"))
-   ((princ num)))
-  )
-(setq numb 1)
-(while (< numb 100) ((fizzbuzz numb)(setq numb (+1 numb)))) 
-
-(defun fizzbuzzl (num)
-  (
-   (cond 
-   ((and (< (mod num 3) 1) (< (mod num 5) 1)) (princ "fizzbuzz"))
-   ((< (mod num 5) 1) (princ "fizz"))
-   ((< (mod num 3) 1) (princ "buzz"))
-   ((princ num)))
-   (setq num (+1 num)) 
-   (fizzbuzzl num)
-  )
-)
-(fizzbuzzl 1)
+  "function demonstrating fizzbuzz in emacs lisp"
+  (interactive "p")
+  (while (< num 101)
+    (cond 
+     ((and (= (mod num 3) 0) (= (mod num 5) 0)) (princ "FIzzbuZZ"))
+     ((= (mod num 3) 0) (princ "fizz"))
+     ((= (mod num 5) 0) (princ "buzz"))
+     ((princ num)))
+    (setq num (1+ num))))
+  
